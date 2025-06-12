cf27877414835391f2f39c07d79034ce174cf6a7
Sam Wolfe <samgwolfe12@gmail.com>
2025-06-12 18:17:51 -0400

    new accessible page

--- src/components/GenerciSite/GenericSite.jsx
+++ src/components/GenerciSite/GenericSite.jsx
@@ -1,13 +1,50 @@
 import wcc from './../../assets/WCC.png';
+import greatreads from './../../assets/goodreads.png';
+import sam from './../../assets/sam.png';
+import title from './../../assets/title.png';
+
 const GenericSite = () => {
     return (
         <div className="generic">
-          <div className="hero">
-            <h1>Hello! I'm Sam Wolfe</h1>
-            <h2>Software Engineer. Front-end Developer.</h2>
+          <div className="hero"  >
+            <div className="left">
+              <img src={title} alt="Sam Wolfe - Software Engineer"></img>
+            </div>
+            <div className="right">
+              <img src={sam} alt="Sam Wolfe"></img>
+            </div>
+            
+
           </div>
           <div className="work">
             <h2>My Work</h2>
+            <div className="card">
+              <a href="index.html" target="_blank" className="" >
+                <div className="front">
+                  <div className="imgContainer">
+                    <img src={greatreads} alt="Greadreads Website"></img>
+                  </div>
+                  <div className="description">
+                  <h2>Greatreads</h2>
+                  <h3>Tech Stack:</h3>
+                  <ul>
+                    <li>Java</li>
+                    <li>React.js</li>
+                    <li>HTML5</li>
+                    <li>CSS#</li>
+                  </ul>
+                  <ul>
+                    <li>PostgreSQL</li>
+                    <li>Railway</li>
+                    <li>Vercel</li>
+                  </ul>
+                  </div>
+                </div>
+                <div className="back">
+                 
+                </div>
+              </a>
+            </div>
             <div className="card">
               <a href="index.html" target="_blank" className="" >
                 <div className="front">
@@ -16,7 +53,12 @@ const GenericSite = () => {
                   </div>
                   <div className="description">
                   <h2>Woodstock Community Church Website</h2>
-                  <p>I made this static site for Woodstock Community Church focusing on accesibility and maintainability.</p>
+                  <h3>Tech Stack:</h3>
+                  <ul>
+                    <li>JavaScript</li>
+                    <li>HTML5</li>
+                    <li>CSS#</li>
+                  </ul>
                   </div>
                 </div>
                 <div className="back">
@@ -24,7 +66,6 @@ const GenericSite = () => {
                 </div>
               </a>
             </div>
-
   
           </div>
         </div>
